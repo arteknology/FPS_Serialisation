d3db4ed18fc28515cbc0d5e6b0ebfa8e3699d251
arteknology <hauton.arthur@gmail.com>
2020-10-09 22:31:06 +0200

    FirstPush

---
+++ Assets/Scripts/Serializable/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Player
+{
+    public string Name;
+    public int Pv;
+    public int Xp;
+    public int Level;
+    public Vector3 Position;
+}
---
+++ Assets/Scripts/Serializable/Weapon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Weapon
+{
+    public int Damage;
+    public int Xp;
+    public int Level;
+}

fd7205626530df4e9f16ffe4d1aa2cb2d560d894
arteknology <hauton.arthur@gmail.com>
2020-10-14 11:55:01 +0200

    PlayerMove

---
+++ Assets/Scripts/Monobehaviours/PlayerMovementControl.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementControl : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float jump_force;
+    [SerializeField] private float jump_raycast_dist;
+
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * _speed;
+
+        Vector3 newPosition = rb.position + rb.transform.TransformDirection(movement);
+        rb.MovePosition(newPosition);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (IsGrounded())
+            {
+                rb.AddForce(0, jump_force, 0, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, jump_raycast_dist);
+    }
+}
